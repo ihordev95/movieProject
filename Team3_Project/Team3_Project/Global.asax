6b58dad53acc733ce75577d04181b365b42c1091
ihordev95 <ihorkozhedubo@outlook.com>
2019-02-07 22:55:54 -0700

    Created project solution.

---
+++ Team3_Project/Team3_Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Team3_Project.MvcApplication" Language="C#" %>
---
+++ Team3_Project/Team3_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Team3_Project.Startup))]
+namespace Team3_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
