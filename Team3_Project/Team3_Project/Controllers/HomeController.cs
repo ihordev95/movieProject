200ad4dfb73cd9f9416f32bd3b39ca71694e340f
ihordev95 <ihorkozhedubo@outlook.com>
2019-02-21 01:47:49 +0000

    Added dropdown so each table can be accsessed.

--- Team3_Project/Team3_Project/Controllers/HomeController.cs
+++ Team3_Project/Team3_Project/Controllers/HomeController.cs
@@ -14,7 +14,31 @@
 		public System.Web.Mvc.ActionResult Contact() {
 			this.ViewBag.Message = "Your contact page.";
 			System.String[] value = this.Request.QueryString.GetValues("table");
-			Database.Movies.TitleBasics  movie = new Database.Movies.TitleBasics();
+			Database.Movies.Abstract movie = new Database.Movies.TitleBasics();
+			if (value != null && value.Length >= 1) {
+				System.String table = value[0];
+				if (table == "0") {
+					movie = new Database.Movies.NameBasics();
+				}
+				if (table == "1") {
+					movie = new Database.Movies.TitleAkas();
+				}
+				if (table == "2") {
+					movie = new Database.Movies.TitleBasics();
+				}
+				if (table == "3") {
+					movie = new Database.Movies.TitleCrew();
+				}
+				if (table == "4") {
+					movie = new Database.Movies.TitleEpisode();
+				}
+				if (table == "5") {
+					movie = new Database.Movies.TitlePrincipals();
+				}
+				if (table == "6") {
+					movie = new Database.Movies.TitleRatings();
+				}
+			}
 			System.Data.DataSet results = movie.SELECT(System.String.Empty, 10);
 			return this.View(results);
 		}
