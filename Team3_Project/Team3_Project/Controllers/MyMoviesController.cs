7d8a5ec18d0aea3766c504b632b394c97bc31005
ihordev95 <ihorkozhedubo@outlook.com>
2019-02-27 20:43:04 -0700

    This is what i did

---
+++ Team3_Project/Team3_Project/Controllers/MyMoviesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Team3_Project.Controllers
+{
+    public class MyMoviesController : Controller
+    {
+        // GET: MyMovies
+        public ActionResult MyMovies()
+        {
+            return View();
+        }
+    }
+}
