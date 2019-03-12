7f379315a49aee5de41079ce65e45dd66a22ebaf
ihordev95 <ihorkozhedubo@outlook.com>
2019-03-11 18:50:15 -0700

    Add browse page

---
+++ Team3_Project/Team3_Project/BrowseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Team3_Project
+{
+    public class BrowseController : Controller
+    {
+        // GET: Browse
+        public ActionResult Browse()
+        {
+            return View();
+        }
+    }
+}
