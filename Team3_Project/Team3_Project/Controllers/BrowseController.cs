bb71f77484189063c8fb40933bacc0943b81f69d
ihordev95 <ihorkozhedubo@outlook.com>
2019-03-14 01:09:30 +0000

    REVERTED to 02/28

--- Team3_Project/Team3_Project/Controllers/BrowseController.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Team3_Project.Controllers {
-	public class BrowseController : System.Web.Mvc.Controller {
-		// GET: Browse
-		public System.Web.Mvc.ActionResult Browse() {
-			return this.View();
-		}
-	}
-}
--- Team3_Project/Team3_Project/Controllers/MyMoviesController.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Team3_Project.Controllers {
-	public class MyMoviesController : System.Web.Mvc.Controller {
-		// GET: MyMovies
-		public System.Web.Mvc.ActionResult MyMovies() {
-			return this.View();
-		}
-	}
-}
