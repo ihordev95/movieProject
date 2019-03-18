1fea844139e7a3e7a879733016d72167f66572b7
ihordev95 <ihorkozhedubo@outlook.com>
2019-03-18 20:37:05 +0000

    Can now insert into 'film' tables from webpage. (Does not prevent SQL injection attack.)

--- Team3_Project/Team3_Project/Controllers/HomeController.cs
+++ Team3_Project/Team3_Project/Controllers/HomeController.cs
@@ -16,11 +16,20 @@
 		public System.Web.Mvc.ActionResult Contact() {
 			this.ViewBag.Message = "Your contact page.";
 			System.Collections.Specialized.NameValueCollection QueryString = this.Request.QueryString;
+			//
+			if (database.url.Boolean(QueryString , "insert_list")) {
+				new database.memdixyp_film.list(QueryString).INSERT();
+			}
+			if (database.url.Boolean(QueryString , "insert_list_film")) {
+				new database.memdixyp_film.list_film(QueryString).INSERT();
+			}
+			if (database.url.Boolean(QueryString , "insert_user")) {
+				new database.memdixyp_film.user(QueryString).INSERT();
+			}
+			//
 			database.database movie = database.database.get(QueryString);
 			System.String where = database.url.String(QueryString , "where");
 			System.Data.DataSet results = movie.SELECT(where , 50);
-			database.memdixyp_film.user user = new database.memdixyp_film.user(QueryString);
-			user.INSERT();
 			return this.View(results);
 		}
 	}
