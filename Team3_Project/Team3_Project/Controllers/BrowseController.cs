a728934591a6eec3517cd7818fad7cc65eaa6709
ihordev95 <ihorkozhedubo@outlook.com>
2019-03-18 18:49:45 -0600

    Added multiple placeholder pages

---
+++ Team3_Project/Team3_Project/Controllers/BrowseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Team3_Project.Controllers
+{
+    public class BrowseController : Controller
+    {
+        // GET: Browse
+        public ActionResult BrowseMovies()
+        {
+            return View();
+        }
+    }
+}
