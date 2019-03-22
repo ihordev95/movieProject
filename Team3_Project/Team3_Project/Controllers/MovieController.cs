86708db1940ecca1951b1b368783b3e8ad4d7e3c
ihordev95 <ihorkozhedubo@outlook.com>
2019-03-21 18:19:34 -0600

    Added watchable class, API class, and basic dynamic page generation with ID in the url

--- Team3_Project/Team3_Project/Controllers/MovieController.cs
+++ Team3_Project/Team3_Project/Controllers/MovieController.cs
@@ -1,7 +1,9 @@
-﻿namespace Team3_Project.Controllers {
+﻿using Newtonsoft.Json;
+
+namespace Team3_Project.Controllers {
 	public class MovieController : System.Web.Mvc.Controller {
 		// GET: Movie
-		public System.Web.Mvc.ActionResult MoviePage() {
+		public System.Web.Mvc.ActionResult MoviePage(string ID = "tt0000001") {
 			Databases.memdixyp_imdb.title_akas title_akas = new Databases.memdixyp_imdb.title_akas();
 			Databases.memdixyp_imdb.title_basics title_basics = new Databases.memdixyp_imdb.title_basics();
 			Databases.memdixyp_imdb.title_episode title_episode = new Databases.memdixyp_imdb.title_episode();
@@ -12,7 +14,13 @@
 			results.Merge(title_basics.SELECT(System.String.Format("tconst='{0}'" , movie)));
 			results.Merge(title_episode.SELECT(System.String.Format("tconst='{0}'" , movie)));
 			results.Merge(title_ratings.SELECT(System.String.Format("tconst='{0}'" , movie)));
-			return this.View(results);
+
+
+            Helpers.ODBAPI api = new Helpers.ODBAPI();
+            string response = api.getIDinfo(ID);
+            Helpers.Watchable w = JsonConvert.DeserializeObject<Helpers.Watchable>(response);
+            ViewBag.json = w;
+            return this.View(results);
 		}
 	}
 }
---
+++ Team3_Project/Team3_Project/Helpers/Watchable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team3_Project.Helpers
+{
+    public class Watchable
+    {
+        public String imdbID;
+        public String title;
+        public String plot;
+        public String longDescription;
+        public String poster;
+        public String type;
+        public String year;
+        public String contentRating;
+        public String runtime;
+        public String genre;
+        public String websiteUrl;
+    }
+}
