2f187d715901a6f58d16191b1192815650156d8c
ihordev95 <ihorkozhedubo@outlook.com>
2019-04-02 20:45:22 +0000

    Saving random stuff I was worknig on. Need to check that it works.

--- Team3_Project/Team3_Project/Controllers/ViewListController.cs
+++ Team3_Project/Team3_Project/Controllers/ViewListController.cs
@@ -4,26 +4,14 @@
 		public System.Web.Mvc.ActionResult ViewList(System.String id = "1") {
 			Databases.type.Int32 identifier = new Databases.type.Int32(1);
 			identifier.parse(id);
-
-			System.String SELECT = Databases.memdixyp_film.list.self.SELECT(System.String.Format("identifier='{0}'" , identifier));
-			Databases.memdixyp_film.list list = (Databases.memdixyp_film.list) Databases.memdixyp_film.list.self.individual(SELECT);
-			this.ViewBag.list = list;
-
-			System.String CALL = Databases.memdixyp_film.view_list.self.CALL();
-			Databases.memdixyp_film.view_list[] view_list = (Databases.memdixyp_film.view_list[]) Databases.memdixyp_film.view_list.self.collection(CALL);
-			this.ViewBag.view_list = view_list;
-			/*
-
-						this.ViewBag.list = Databases.memdixyp_film.list.select(System.String.Format("identifier='{0}'" , identifier));
-						Databases.memdixyp_film.view_list view_list = new Databases.memdixyp_film.view_list(identifier.value);
-						System.Data.DataSet result = view_list.CALL();
-						return this.View(result);
-						*/
+			System.String SELECT = Databases.memdixyp_film.list.self.SELECT(System.String.Format("`identifier`='{0}'" , identifier));
+			this.ViewBag.list = Databases.memdixyp_film.list.individual(SELECT);
+			this.ViewBag.view_list = Databases.memdixyp_film.view_list.collection(identifier.value);
 			return this.View();
 		}
 		public System.String AddList() {
 			Databases.memdixyp_film.list_movie list_movie = new Databases.memdixyp_film.list_movie(this.Request.QueryString);
-			list_movie.INSERT();
+			Databases.memdixyp_film.list_movie.individual(list_movie.INSERT());
 			return "alert message";
 		}
 	}
