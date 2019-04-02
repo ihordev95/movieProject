010aeba54a23a5a365e44ed8ed7f170b297a05ea
ihordev95 <ihorkozhedubo@outlook.com>
2019-04-01 20:35:10 -0600

    Added the Profile, Manage List, and Available List pages.

---
+++ Team3_Project/Team3_Project/Controllers/AvailableListViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Team3_Project.Controllers
+{
+    public class AvailableListViewController : Controller
+    {
+        // GET: AvailableListView
+        public ActionResult AvailableLists()
+        {
+            return View();
+        }
+    }
+}
