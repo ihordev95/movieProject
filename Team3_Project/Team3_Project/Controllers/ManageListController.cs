2b149c6878700a8aad214578e31c57479ad2eb3b
ihordev95 <ihorkozhedubo@outlook.com>
2019-04-06 20:39:31 +0000

    Added list loading and checkboxes to manage list page.

--- Team3_Project/Team3_Project/Controllers/ManageListController.cs
+++ Team3_Project/Team3_Project/Controllers/ManageListController.cs
@@ -1,7 +1,10 @@
 ﻿namespace Team3_Project.Controllers {
 	public class ManageListController : System.Web.Mvc.Controller {
 		// GET: ManageList
-		public System.Web.Mvc.ActionResult ManageLists() {
+		public System.Web.Mvc.ActionResult ManageLists(System.String id = "1") {
+			Databases.type.Int32 identifier = new Databases.type.Int32(1);
+			identifier.parse(id);
+			this.ViewBag.view_list = Databases.memdixyp_film.view_list.show_movie_list(identifier);
 			return this.View();
 		}
 	}
