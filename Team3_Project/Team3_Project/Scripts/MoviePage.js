5b604983e4d71821389ff9fa57182e41b1acdd1f
ihordev95 <ihorkozhedubo@outlook.com>
2019-04-08 20:37:52 +0000

    Formating tweaks.

--- Team3_Project/Team3_Project/Scripts/MoviePage.js
+++ Team3_Project/Team3_Project/Scripts/MoviePage.js
@@ -3,8 +3,7 @@
     browser:true, devel:true, long:true
 */
 /*global
-    ajax_failure, ajax_get, ajax_join, ajax_prepare, ajax_succeed, alert,
-    document, event_click_one_run
+    ajax_failure, ajax_get, ajax_join, ajax_prepare, ajax_succeed, alert, document, event_click_one_run
 */
 /*property
     display, getElementById, style
@@ -14,32 +13,30 @@ function ajax_prepare(ignore) {
     var parameter = [];
     parameter = ajax_join(parameter, "list");
     parameter = ajax_join(parameter, "movie");
-    //    parameter = ajax_join(parameter, "poster");
     return parameter;
 }
 function ajax_succeed(data) {
     "use strict";
     alert(data);
-    /*
-    var form = document.getElementById(data[0]);
-    var list = form.elements;
-    list.is_active.value = data[1];
-    date_succeed(list.start_date, data[2]);
-    date_succeed(list.end_date, data[3]);
-    return form;
-    */
 }
 function ajax_failure(code, text) {
     "use strict";
     alert(code + " : " + text);
 }
 event_click_one_run("#save", function (ignore) {
+    "use strict";
     document.getElementById("light").style.display = "none";
     document.getElementById("fade").style.display = "none";
     var form = document.getElementById("form");
     ajax_get("/ViewList/AddList", form);
 });
 event_click_one_run("#cancel", function (ignore) {
+    "use strict";
     document.getElementById("light").style.display = "none";
     document.getElementById("fade").style.display = "none";
+});
+event_click_one_run("#test1_save", function (ignore) {
+    "use strict";
+    document.getElementById("light").style.display = "block";
+    document.getElementById("fade").style.display = "block";
 });
