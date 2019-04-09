99a2d55fefbb84585f6a32b20c5ba1483a010027
ihordev95 <ihorkozhedubo@outlook.com>
2019-04-08 23:43:24 -0600

    styled Register and Login, added popup for creating new list, created table for curated lists, added noimageavailable link

---
+++ Team3_Project/Team3_Project/Scripts/ProfilePage.js
@@ -0,0 +1,43 @@
+/* JSLint edition 2019-01-31 */
+/*jslint
+    browser:true, devel:true, long:true
+*/
+/*global
+    ajax_failure, ajax_get, ajax_join, ajax_prepare, ajax_succeed, alert, document, event_click_one_run
+*/
+/*property
+    display, getElementById, style
+*/
+function ajax_prepare(ignore) {
+    "use strict";
+    var parameter = [];
+    parameter = ajax_join(parameter, "list");
+    parameter = ajax_join(parameter, "movie");
+    return parameter;
+}
+function ajax_succeed(data) {
+    "use strict";
+    alert(data);
+}
+function ajax_failure(code, text) {
+    "use strict";
+    alert(code + " : " + text);
+}
+event_click_one_run("#save", function (ignore) {
+    "use strict";
+    document.getElementById("light").style.display = "none";
+    document.getElementById("fade").style.display = "none";
+    var form = document.getElementById("form");
+    //THIS NEEDS TO BE UPDATED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ajax_get("/ViewList/AddList", form);
+});
+event_click_one_run("#cancel", function (ignore) {
+    "use strict";
+    document.getElementById("light").style.display = "none";
+    document.getElementById("fade").style.display = "none";
+});
+event_click_one_run("#test1_create", function (ignore) {
+    "use strict";
+    document.getElementById("light").style.display = "block";
+    document.getElementById("fade").style.display = "block";
+});
