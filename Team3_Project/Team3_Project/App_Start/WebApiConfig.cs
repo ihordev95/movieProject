0c05cae4545e336e16b74dcebe2756f8d8667250
ihordev95 <ihorkozhedubo@outlook.com>
2019-04-17 17:18:43 -0600

    Fixed configuration issue to default to json

--- Team3_Project/Team3_Project/App_Start/WebApiConfig.cs
+++ Team3_Project/Team3_Project/App_Start/WebApiConfig.cs
@@ -3,8 +3,11 @@ namespace Team3_Project {
 	public static class WebApiConfig {
 		public static void Register(System.Web.Http.HttpConfiguration config) {
 			config.MapHttpAttributeRoutes();
-			config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
-			config.Routes.MapHttpRoute(
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings.Add(new System.Net.Http.Formatting.RequestHeaderMapping("Accept","text/html",
+                              System.StringComparison.InvariantCultureIgnoreCase,
+                              true,
+                              "application/json"));
+            config.Routes.MapHttpRoute(
 				name: "DefaultApi" ,
 				routeTemplate: "api/{controller}/{id}" ,
 				defaults: new { id = System.Web.Http.RouteParameter.Optional }
